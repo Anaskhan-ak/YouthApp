86556fa353327dd0698dbc2044c49bf0a080adf0
Sannya Wasim <sannya.wasim01@gmail.com>
2025-04-24 17:40:19 +0500

    resp in Notifications

--- src/screens/landingWidget/components/Notifications.jsx
+++ src/screens/landingWidget/components/Notifications.jsx
@@ -1,130 +1,123 @@
-import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Myfont } from '../../../assets/fonts/Fonts';
 import { DropDownIcon } from '../../../assets/images/svgs';
-import { width } from '../../../constant';
+import { height, width } from '../../../constant';
 import { colors } from '../../../utils/colors';
 import SwipeableList from './Swippable';
 
-  const Notifications = ({
-    title,
-    toggle,
-    setToggle,
-    isVisible,
-    setVisibility,
-  }) => {
-    if (!isVisible) {return null;}
-    return (
-      <>
-        <View style={styles?.container}>
-          {title && (
-            <View style={styles?.content}>
-              <Text style={styles.welcomeTextz}>{title}</Text>
-              <View style={styles?.buttonContainer}>
-                <TouchableOpacity
-                  onPress={() => {
-                    setToggle(!toggle);
-                  }}
-                  style={styles?.dropdownButton}>
-                  <View
-                    style={{transform: [{rotate: toggle ? '0deg' : '180deg'}]}}>
-                    <DropDownIcon />
-                  </View>
-                  <Text
-                    style={styles?.dropdownText}>
-                    {toggle === false ? 'Show more' : 'Show less'}
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles?.crossButton}
-                  onPress={() => setVisibility(false)}>
-                  <Text
-                    style={styles?.crossButtonText}>
-                    X
-                  </Text>
-                </TouchableOpacity>
-              </View>
+const Notifications = ({
+  title,
+  toggle,
+  setToggle,
+  isVisible,
+  setVisibility,
+}) => {
+  if (!isVisible) {
+    return null;
+  }
+  return (
+    <>
+      <View style={styles?.container}>
+        {title && (
+          <View style={styles?.content}>
+            <Text style={styles.welcomeTextz}>{title}</Text>
+            <View style={styles?.buttonContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  setToggle(!toggle);
+                }}
+                style={styles?.dropdownButton}>
+                <View
+                  style={{transform: [{rotate: toggle ? '0deg' : '180deg'}]}}>
+                  <DropDownIcon />
+                </View>
+                <Text style={styles?.dropdownText}>
+                  {toggle === false ? 'Show more' : 'Show less'}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles?.crossButton}
+                onPress={() => setVisibility(false)}>
+                <Text style={styles?.crossButtonText}>X</Text>
+              </TouchableOpacity>
             </View>
-          )}
-        </View>
-        {/* Notification bars */}
-        <View style={{
-          width : '100%',
-          justifyContent : 'center',
-          alignItems : 'center'
-        }}>
-          {toggle ? (
-            <TouchableOpacity
-              style={{width : width*0.9}}
-              onPress={() => {
-                setToggle(false);
-              }}>
-              <SwipeableList setVisibility={setVisibility} />
-              {/* <SwipeableList/> */}
-              {/* <Text>QQQQQQQQ</Text> */}
-            </TouchableOpacity>
-          ) : null}
-        </View>
-      </>
-    );
-  };
+          </View>
+        )}
+      </View>
+      {/* Notification bars */}
+      <View style={styles?.notificationContainer}>
+        {toggle ? (
+          <TouchableOpacity
+            style={styles?.notificationButton}
+            onPress={() => {
+              setToggle(false);
+            }}>
+            <SwipeableList setVisibility={setVisibility} />
+          </TouchableOpacity>
+        ) : null}
+      </View>
+    </>
+  );
+};
 
-  export default Notifications;
+export default Notifications;
 
-  const styles = StyleSheet?.create({
-    container: {
-      width: width,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 4,
-      marginBottom: 8,
-    },
-    content: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      width: '87%',
-    },
-    buttonContainer: {
-      flexDirection: 'row',
-      columnGap: 8,
-      alignItems: 'center',
-    },
-    dropdownButton: {
-      backgroundColor: 'rgba(250, 250, 250, 0.34)',
-      padding: width * 0.0085,
-      paddingHorizontal: width * 0.02,
-      borderRadius: 25,
-      columnGap: 3,
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-    dropdownText : {
-      fontSize: width * 0.023,
-      fontFamily: Myfont.Regular,
-      color: colors.black,
-    },
-    welcomeTextz: {
-      color: 'rgba(250, 250, 250, 0.75)',
-      fontSize: width * 0.039,
-      fontFamily: Myfont?.ExtraBold,
-    },
-    crossButton : {
-      backgroundColor: 'rgba(250, 250, 250, 0.34)',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width : 20,
-      height : 20,
-      borderRadius : 30,
-    },
-    crossButtonText : {
-      fontFamily: Myfont.Regular,
-      fontSize: 9,
-      textAlign: 'center',
-      color: colors.text,
-    },
-  });
+const styles = StyleSheet?.create({
+  container: {
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: height * 0.003,
+    marginBottom: height * 0.007,
+  },
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: width * 0.87,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    columnGap: height * 0.007,
+    alignItems: 'center',
+  },
+  dropdownButton: {
+    backgroundColor: 'rgba(250, 250, 250, 0.34)',
+    padding: width * 0.0085,
+    paddingHorizontal: width * 0.02,
+    borderRadius: width * 0.1,
+    columnGap: width * 0.01,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  dropdownText: {
+    fontSize: width * 0.023,
+    fontFamily: Myfont.Regular,
+    color: colors.black,
+  },
+  welcomeTextz: {
+    color: 'rgba(250, 250, 250, 0.75)',
+    fontSize: width * 0.039,
+    fontFamily: Myfont?.ExtraBold,
+  },
+  crossButton: {
+    backgroundColor: 'rgba(250, 250, 250, 0.34)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: width * 0.04,
+    height: width * 0.04,
+    borderRadius: width * 0.04/2,
+  },
+  crossButtonText: {
+    fontFamily: Myfont.Regular,
+    fontSize: width * 0.025,
+    textAlign: 'center',
+    color: colors.text,
+  },
+  notificationContainer: {
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  notificationButton: {width: width * 0.9},
+});
